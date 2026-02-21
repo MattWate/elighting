d5d3f37e3f9dec43f59bcbe43d2b49d0511d65dd
MattWate <matthew@dubyahinnovation.com>
2026-02-21 13:49:09 +0200

    Update layout.tsx

--- e-lighting/src/app/layout.tsx
+++ e-lighting/src/app/layout.tsx
@@ -5,10 +5,14 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#0a0a0a]`}>
+      <body className={`${inter.className} bg-[#0a0a0a] text-slate-100 antialiased`}>
         <Navbar />
         {children}
       </body>
