a543920ba65f5ab911b39e43cf7d75c12d01d68d
MattWate <matthew@dubyahinnovation.com>
2026-02-21 14:00:14 +0200

    Update tailwind.config.js

--- e-lighting/tailwind.config.js
+++ e-lighting/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // This covers everything in your src folder
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
